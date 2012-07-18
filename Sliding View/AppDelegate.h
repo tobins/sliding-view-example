d1ca93590a7e5faa4c18a8efe6279a87a61e16ec
Tobin Schwaiger-Hastanan <tobins@gmail.com>
2012-07-18 12:25:53 -0400

    Initial Commit

---
+++ Sliding View/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Sliding View
+//
+//  Created by Tobin Schwaiger-Hastanan on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
